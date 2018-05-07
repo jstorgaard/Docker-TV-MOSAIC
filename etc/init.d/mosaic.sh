382bad9743270dfbc1ef9d933684642bee82f534
Philip Lawall <fueller@mail.lawall.me>
2018-05-07 10:36:25 +0200

    fixed docker restart files

--- etc/init.d/mosaic.sh
+++ etc/init.d/mosaic.sh
@@ -2,6 +2,32 @@
 
 chmod -R /opt/tvmosaiv
 chmod -R /usr/local/bin/tvmosaic
+chmod -R 777 /opt-start/TVMosaic
+chmod -R 777 /opt-start/tvmosaic
+
+
+if [ -z "`la /usr/local/bin/tvmosaic --hide='lost+found'`" ]
+then
+    cp -R /opt-start/tvmosaic/* /usr/local/bin/tvmosaic
+fi
+if [ ! -f "/usr/local/bin/tvmosaic/start.sh" ]
+then
+    cp -Rn /opt-start/tvmosaic/* /usr/local/bin/tvmosaic/
+fi
+
+if [ -z "`ls /opt/TVMosaic --hide='lost+found'`" ]
+then
+    cp -R /opt-start/TVMosaic/* /opt/TVMosaic
+fi
+if [ ! -f "/opt/TVMosaic/recorder_database/recorder_database.db" ]
+then
+    cp -Rn /opt-start/TVMosaic/* /opt/TVMosaic/
+fi
 
 /usr/local/bin/tvmosaic/start.sh
 bash
+
+else
+echo "Install Wrong! Please Check Image or Path Config!"
+sleep 60
+fi
